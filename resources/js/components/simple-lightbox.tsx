e6bfcbe2b12301d87d6e2c1efe3dff7462418481
dclarktech302 <dcps302@gmail.com>
2026-01-23 13:12:12 -0500

    embed google form, switching images to 3 cols

--- resources/js/components/simple-lightbox.tsx
+++ resources/js/components/simple-lightbox.tsx
@@ -93,7 +93,7 @@ export default function SimpleLightbox() {
 
   return (
     <>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-4">
         {images.map((image, index) => (
           <img
             key={index}
