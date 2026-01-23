0aa8e7d697385a8b7cd567ebf855e1f12b46bc81
dclarktech302 <dcps302@gmail.com>
2026-01-23 16:10:45 -0500

    update footer icons and links to socials

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -40,24 +40,63 @@ export default function FooterSection() {
                         </Link>
                     ))}
                 </div>
-                <div className="my-8 flex flex-wrap justify-center gap-6 text-sm">
-                    <a
-                        href="https://www.instagram.com/ghostgangmusicgroup/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label="Instagram"
-                        className="text-muted-foreground hover:text-primary block">
-                        <svg
-                            className="size-6"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="1em"
-                            height="1em"
-                            viewBox="0 0 24 24">
-                            <path
-                                fill="currentColor"
-                                d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"></path>
-                        </svg>
-                    </a>
+                <div className="my-8 flex flex-wrap justify-center gap-6">
+                    <div>
+                        <a
+                            href="https://www.facebook.com/share/g/1A3fE4Uqfn/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Facebook"
+                            className="text-muted-foreground hover:text-primary block">
+                            <img
+                                className="size-6"
+                                src="/images/Facebook_Logo_Primary.png"
+                                alt="Facebook"
+                            />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://youtube.com/@ghostgangrecordsmusicgroup?si=hm_hn8ka57ahLE0M"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="YouTube"
+                            className="text-muted-foreground hover:text-primary block">
+                            <img
+                                className="size-6"
+                                src="/images/yt_icon_red_digital.png"
+                                alt="YouTube"
+                            />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://on.soundcloud.com/dmoR0NnBPvOd3XlYPK"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="SoundCloud"
+                            className="text-muted-foreground hover:text-primary block">
+                            <img
+                                className="size-6"
+                                src="/images/6862460f240ad9ae4680f211_cloudmark-white.png"
+                                alt="SoundCloud"
+                            />
+                        </a>
+                    </div>
+                    <div>
+                        <a
+                            href="https://www.instagram.com/ghostgangmusicgroup/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Instagram"
+                            className="text-muted-foreground hover:text-primary block">
+                            <img
+                                className="size-6"
+                                src="/images/Instagram_Glyph_Gradient.png"
+                                alt="Instagram"
+                            />
+                        </a>
+                    </div>
                 </div>
                 <span className="text-muted-foreground block text-center text-sm"> © {new Date().getFullYear()} GGR Music Group, All rights reserved</span>
             </div>
